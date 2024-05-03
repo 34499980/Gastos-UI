5ad8749ff1248e061df533429377c6971f568f15
Ariel Brenman <abrenman@mvfactory.com>
2024-05-03 18:01:07 -0300

    muestro por pantalla

--- src/app/modules/Categories/categories-list/categories-list.component.ts
+++ src/app/modules/Categories/categories-list/categories-list.component.ts
@@ -1,6 +1,9 @@
 
+import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit, Inject, inject } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { Category } from '../../../models/models';
 import { CategryService } from '../../../services/category.service';
 
 @Component({
@@ -8,15 +11,16 @@ import { CategryService } from '../../../services/category.service';
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss'],
   standalone: true,
-  imports: []
+  imports: [MatListModule, NgFor, NgIf]
 })
 export default class CategoriesListComponent implements OnInit {
   categoryService = inject(CategryService);
-
+  list: Category[];
   ngOnInit(): void {
     console.log('Entra')
     this.categoryService.getAll().subscribe({
       next: res => {
+        this.list = res;
         console.log(res)
       } 
     })
