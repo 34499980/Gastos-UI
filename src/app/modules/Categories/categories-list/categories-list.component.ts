76291bfb9bbddccff564457c7dfc6c133eac94f0
Ariel Brenman <abrenman@mvfactory.com>
2024-05-06 13:00:05 -0300

    add dialog

--- src/app/modules/Categories/categories-list/categories-list.component.ts
+++ src/app/modules/Categories/categories-list/categories-list.component.ts
@@ -8,19 +8,23 @@ import { CategryService } from '../../../services/category.service';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogConfirm } from '../../../dialogs/confirm/dialog-confirm';
 
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss'],
   standalone: true,
-  imports: [MatListModule, NgFor, NgIf,MatButtonModule, MatTableModule, MatIconModule]
+  imports: [MatListModule, NgFor, NgIf,MatButtonModule, MatTableModule, MatIconModule, MatDialogModule ]
 })
 export default class CategoriesListComponent implements OnInit {
   categoryService = inject(CategryService);
+  public dialogService = inject(MatDialog);
   list: Category[];
   dataSource = new MatTableDataSource<Category>();
   displayedColumns: string[] = ['image', 'name', 'actions'];
+
   ngOnInit(): void {
     console.log('Entra')
     this.categoryService.getAll().subscribe({
@@ -29,5 +33,18 @@ export default class CategoriesListComponent implements OnInit {
       } 
     })
   }
+  delete(row: Category){
+    const dialogRef = this.dialogService.open(DialogConfirm, {
+      data: {name: row.name, object: "categoria"},
+      
+      disableClose: true
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+     if(result){
+
+     }
+    });
+  }
+  
 }
