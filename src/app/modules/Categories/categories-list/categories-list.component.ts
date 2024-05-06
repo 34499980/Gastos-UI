9ff4c3b8d8342d704f6a92d2b1df59a7813123b5
Ariel Brenman <abrenman@mvfactory.com>
2024-05-06 09:43:42 -0300

    add tailwind and table

--- src/app/modules/Categories/categories-list/categories-list.component.ts
+++ src/app/modules/Categories/categories-list/categories-list.component.ts
@@ -5,23 +5,27 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Category } from '../../../models/models';
 import { CategryService } from '../../../services/category.service';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss'],
   standalone: true,
-  imports: [MatListModule, NgFor, NgIf]
+  imports: [MatListModule, NgFor, NgIf,MatButtonModule, MatTableModule, MatIconModule]
 })
 export default class CategoriesListComponent implements OnInit {
   categoryService = inject(CategryService);
   list: Category[];
+  dataSource = new MatTableDataSource<Category>();
+  displayedColumns: string[] = ['image', 'name', 'actions'];
   ngOnInit(): void {
     console.log('Entra')
     this.categoryService.getAll().subscribe({
       next: res => {
-        this.list = res;
-        console.log(res)
+        this.dataSource.data = res;
       } 
     })
   }
