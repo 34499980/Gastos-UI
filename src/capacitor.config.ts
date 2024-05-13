db5c650c223f1a9e136390f23b263b3c29605a47
Ariel Brenman <abrenman@mvfactory.com>
2024-05-13 17:52:50 -0300

    change title

---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gastosAngular.app',
+  appName: 'gastosAngular',
+  webDir: 'dist/gastosAngular'
+};
+
+export default config;
